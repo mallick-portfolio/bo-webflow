f62d9ed916ac55b81fb57f9b9b49febccad737cb
mallick-portfolioE <tamal1714019@gmail.com>
2022-07-18 13:52:14 +0600

    update category style

--- src/components/CategoryParallex.jsx
+++ src/components/CategoryParallex.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const CategoryParallex = () => {
   return (
     <div class="relative w-screen overflow-x-hidden">
-      <div class="section"></div>
+      <div class="section  flex items-center justify-center">
+        <div className="relative border-b-4 border-white">
+          <h4 className="absolute -top-4 -right-4 text-xl font-serif text-white">
+            <span className="text-2xl">2</span>/6
+          </h4>
+          <h1 className="text-7xl text-white font-semibold">Fashion</h1>
+        </div>
+      </div>
       <div class="section"></div>
       <div class="section"></div>
       <div class="section"></div>
