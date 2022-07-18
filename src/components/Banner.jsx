a36ab50e72a753142a10c03d5aebc10ebeb98d63
mallick-portfolioE <tamal1714019@gmail.com>
2022-07-18 10:44:03 +0600

    update banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import rotate from "../assets/images/rotate.png";
+import { FiInstagram } from "react-icons/fi";
+import { GrTwitter } from "react-icons/gr";
+import { FaLinkedinIn, FaFacebookF } from "react-icons/fa";
 const Banner = () => {
   return (
-    <div className="px-12 mx-auto">
-      <h4 className="text-lg font-medium text-secondary">Hello people!</h4>
-      <div className="flex justify-between items-center mt-8">
-        <h1 className="text-primary text-[8vw] leading-none font-bold">
-          We're creative digital studio.
-        </h1>
-        <div className="mr-36 relative hover:bg-[#a1c4e4] duration-1000 cursor-pointer rounded-full p-4">
-          <img className="w-36  animate-spin-slow" src={rotate} alt="" />
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-5 h-7 rounded-lg border-2 border-primary ">
-            <div className="animate-bounce absolute w-[2px] h-2 rounded-md bg-primary left-[6px] top-[8px]">
-              {""}
-            </div>
+    <div className="px-12 py-8 mx-auto">
+      <h4 className="text-lg font-medium text-secondary mb-8">Hello people!</h4>
+      <div className="flex justify-between items-center">
+        <h1 className="banner-title">We're creative digital studio.</h1>
+        <div className="banner-image">
+          <img
+            className="w-36 relative z-40 animate-spin-slow"
+            src={rotate}
+            alt=""
+          />
+          <div className="banner-bg">
+            <div className="banner-hover">{""}</div>
           </div>
         </div>
+      </div>
+      <div className="flex justify-center items-center mt-4">
+        <ul className="flex">
+          <li className="social-item">
+            <FiInstagram className="social-icon" />
+          </li>
+          <li className="social-item">
+            <GrTwitter className="social-icon" />
+          </li>
+          <li className="social-item">
+            <FaLinkedinIn className="social-icon" />
+          </li>
+          <li className="social-item">
+            <FaFacebookF className="social-icon" />
+          </li>
+        </ul>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,11 @@ module.exports = {
       colors: {
         primary: "#000",
         secondary: "#6e6e6e",
+        accent: "#a1c4e4",
       },
       animation: {
-        'spin-slow': 'spin 5s linear infinite',
-      }
+        "spin-slow": "spin 5s linear infinite",
+      },
     },
   },
   plugins: [],
