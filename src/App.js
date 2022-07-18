2f6ecab6912780208fb21bcb4f17df70c8a542c5
mallick-portfolioE <tamal1714019@gmail.com>
2022-07-18 14:06:38 +0600

    updaate category style

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/Home/Home.jsx";
 import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    <div className="overflow-hidden">
+    <div className="">
       <Header />
       <Routes>
         <Route path={"/"} element={<Home />} />
--- src/components/CategoryParallex.jsx
+++ src/components/CategoryParallex.jsx
@@ -2,22 +2,48 @@ import React from "react";
 
 const CategoryParallex = () => {
   return (
-    <div class="relative w-screen overflow-x-hidden">
-      <div class="section  flex items-center justify-center">
-        <div className="relative border-b-4 border-white">
-          <h4 className="absolute -top-4 -right-4 text-xl font-serif text-white">
-            <span className="text-2xl">2</span>/6
-          </h4>
-          <h1 className="text-7xl text-white font-semibold">Fashion</h1>
+    <div class="sectionContainer">
+      {data.map((d) => (
+        <div
+          key={d.id}
+          class="section sticky top-0  flex items-center justify-center"
+        >
+          <div className="relative border-b-4 border-white">
+            <h4 className="absolute -top-4 -right-4 text-xl font-serif text-white">
+              <span className="text-2xl">{d.id}</span>/{data.length}
+            </h4>
+            <h1 className="text-7xl text-white font-semibold">{d.title}</h1>
+          </div>
         </div>
-      </div>
-      <div class="section"></div>
-      <div class="section"></div>
-      <div class="section"></div>
-      <div class="section"></div>
-      <div class="section"></div>
+      ))}
     </div>
   );
 };
 
+const data = [
+  {
+    id: 1,
+    title: "Fashion",
+  },
+  {
+    id: 2,
+    title: "Magazine",
+  },
+  {
+    id: 3,
+    title: "Black Apple Watch",
+  },
+  {
+    id: 4,
+    title: "Ivor Application",
+  },
+  {
+    id: 5,
+    title: "Flying to the Moon",
+  },
+  {
+    id: 6,
+    title: "Drama Webfont",
+  },
+];
 export default CategoryParallex;
